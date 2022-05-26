911ffe1f19cf9cf4dc56eb299c0820269555384d
Young Jae Kim <youngjaekim22@naver.com>
2022-05-26 12:30:30 +0900

    Test2

--- app/src/main/java/com/capstone/oan/MainActivity.java
+++ app/src/main/java/com/capstone/oan/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // test2
+    // test23
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
